2ede5fa9f4c235b7a82fcca5f0ff464143018596
Nicolas Aguero <chivinic30@gmail.com>
2024-08-01 21:10:34 -0300

    xd

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let opcion = parseInt(prompt(`Bienvenido a la tienda, ingrese una opcion:
+    
+    1- Ver todo el stock.
+    2- Buscar por liga.
+    3- Buscar por equipo.
+    4- Comprar.
+    
+    0-SALIR.`))
+
+    
